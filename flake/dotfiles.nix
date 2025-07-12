cbee565794f72ad09d56cb8dc7e259538d2a1bb7
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2025-07-12 15:28:22 -0400

    add nix config

--- flake/dotfiles.nix
+++ flake/dotfiles.nix
@@ -1,4 +1,5 @@
 {
   ".config/hypr/".source = ././dotfiles/hypr;
   ".config/foot/".source = ././dotfiles/foot;
+  ".config/nix/".source = ././dotfiles/nix;
 }
--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
 # This file will be used to install things such as home-manager, my home-manager config, and Nix itself.
+
+# home-manager installation
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+home-manager switch --flake ~/setup
