c1f5798cedd54d78809928d1bf45fa6a42379089
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2025-07-21 23:38:41 -0400

    add git and home-manager command aliases

--- flake/modules/aliases.nix
+++ flake/modules/aliases.nix
@@ -4,10 +4,20 @@
 		# nxs = "home-manager switch --flake ~/setup/flake";
 		
 		# check home-manager flake
-		nfc = "nix flake check ~/setup/flake";
+		hmc = "nix flake check ~/setup/flake";
+		# inspect home-manager flake in nix repl
+		hmi = "nix repl --expr \"builtins.getFlake \\\"/home/purplepentagons/setup/flake\\\"\"";
+		# nix-shell -p <package>
+		nsp = "nix-shell -p";
 
-		# inspect home-manager flake
-		nfi = "nix repl --expr \"builtins.getFlake \\\"/home/purplepentagons/setup/flake\\\"\"";
+		# git commit -m <message>
+		gcm = "git commit -m";
+		# git push
+		gps = "git push";
+		# git clone <repo link>
+		gcl = "git clone";
+		# find file in git repo
+		gfn = "git ls-files | grep";
 
 		ls = "eza --icons";
 	};
