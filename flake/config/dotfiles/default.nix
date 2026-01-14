39de44d451f2a2c144d635ea8b5a6018569ffaf1
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2026-01-13 20:38:39 -0500

    configure dotfiles module to contain my dotfiles

--- flake/config/dotfiles/default.nix
+++ flake/config/dotfiles/default.nix
@@ -1,3 +1,14 @@
 {
-	
+	modules.dotfiles = {
+		folders = {
+			"hypr" = ./hypr;
+			"fuzzel" = ./fuzzel;
+			"foot" = ./foot;
+			"waybar" = ./waybar;
+			"mako" = ./mako;
+		};
+		files = {
+			"starship.toml" = ./no_folder/starship.toml.mustache;
+		};
+	};
 }
