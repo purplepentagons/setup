e2d636ad31ee8c053bee5dbd22715b7f4b8e3c7a
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2026-01-17 17:03:27 -0500

    fix wallpapers not working on virtual machine

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 # This file will be used to install things such as home-manager, my home-manager config, and Nix itself.
 
+# It does not currently work.
+pacman -S nix
+systemctl enable nix-daemon.service
+systemctl start nix-daemon.service
+
 # home-manager installation
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
-home-manager switch --flake ~/setup --extra-experimental-features "nix-command flakes"
+home-manager switch --flake ~/setup/flake --extra-experimental-features "nix-command flakes pipe-operators" -b backup
